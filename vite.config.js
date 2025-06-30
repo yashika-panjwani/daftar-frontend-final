6d7b328fdeafdbb77973095553032ef43c05cdad
yashika-panjwani <yashika.panjwani2021@vitbhopal.ac.in>
2025-06-30 22:41:39 +0530

    Fix: set base to / for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-   base: '/daftar-webiste.github.io/',
+  base: '/', // ✅ IMPORTANT: fix for Vercel
   assetsInclude: ['**/*.JPG'],
   plugins: [
     react(),
     tailwindcss(),
   ],
-  server: {port:5173}
+  server: { port: 5173 }
 })
-
-
